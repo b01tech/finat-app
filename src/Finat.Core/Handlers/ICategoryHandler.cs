3b73984031f787f90f2e2a9ecf9e4ba6114a1693
b01tech <bruno01.tech@gmail.com>
2026-01-09 14:15:28 -0300

    feat: add category handler interface

---
+++ src/Finat.Core/Handlers/ICategoryHandler.cs
@@ -0,0 +1,14 @@
+﻿using Finat.Core.Models;
+using Finat.Core.Requests.Categories;
+using Finat.Core.Responses;
+
+namespace Finat.Core.Handlers;
+
+public interface ICategoryHandler
+{
+    Task<Response<Category>> CreateCategoryAsync(CreateCategoryRequest request);
+    Task<Response<Category>> UpdateCategoryAsync(UpdateCategoryRequest request);
+    Task<Response<Category>> DeleteCategoryAsync(DeleteCategoryRequest request);
+    Task<Response<Category>> GetCategoryByIdAsync(GetByIdCategoryRequest request);
+    Task<Response<List<Category>>> GetAllCategoriesAsync(GetAllCategoryRequest request);
+}
