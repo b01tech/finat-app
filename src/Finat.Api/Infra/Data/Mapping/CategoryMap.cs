f2308c1320760cfaf3b6a94ebdad896bb6263090
b01tech <bruno01.tech@gmail.com>
2026-01-09 11:00:51 -0300

    feat(infra): add models mapping to ef

---
+++ src/Finat.Api/Infra/Data/Mapping/CategoryMap.cs
@@ -0,0 +1,21 @@
+﻿using Finat.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Finat.Api.Infra.Data.Mapping;
+
+public class CategoryMap: IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Category");
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Title)
+            .IsRequired(true)
+            .HasMaxLength(80);
+        builder.Property(c => c.Description)
+            .HasMaxLength(200);
+        builder.Property(c => c.UserId)
+            .IsRequired(true);
+    }
+}
---
+++ src/Finat.Api/Infra/Data/Mapping/TransactionMap.cs
@@ -0,0 +1,25 @@
+﻿using Finat.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Finat.Api.Infra.Data.Mapping;
+
+public class TransactionMap : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.ToTable("Transaction");
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Title)
+            .IsRequired(true)
+            .HasMaxLength(80);
+        builder.Property(t => t.Amount)
+            .IsRequired(true);
+        builder.Property(t => t.Type)
+            .IsRequired(true);
+        builder.Property(t => t.PaidOrReceivedAt)
+            .IsRequired(false);
+        builder.Property(t => t.UserId)
+            .IsRequired(true);
+    }
+}
