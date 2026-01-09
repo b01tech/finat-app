50d8c05e7a7f1fc81a21525383510590b7d14ce9
b01tech <bruno01.tech@gmail.com>
2026-01-09 11:31:12 -0300

    feat(infra): add dbcontext to contatiner di

---
+++ src/Finat.Api/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Finat.Api.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finat.Api.Extensions;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("PostgresConnection") ?? string.Empty;
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        return services;
+    }
+}
--- src/Finat.Api/Program.cs
+++ src/Finat.Api/Program.cs
@@ -2,7 +2,9 @@ using Finat.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDocumentationApi();
+builder.Services
+    .AddDocumentationApi()
+    .AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 app.UseSwaggerDocs();
