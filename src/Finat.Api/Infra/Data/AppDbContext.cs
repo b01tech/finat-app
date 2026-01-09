395cf266c1de0d3813dd2559566b3996716ca1dc
b01tech <bruno01.tech@gmail.com>
2026-01-09 10:49:12 -0300

    feat: add AppDbContext

---
+++ src/Finat.Api/Infra/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Finat.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finat.Api.Infra.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Transaction> Transactions { get; set; } = null!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+
+}
