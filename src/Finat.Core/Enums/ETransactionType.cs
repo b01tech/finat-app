ecf02769e2dfd0440a5860eebeb45f7e43f1f5d1
b01tech <bruno01.tech@gmail.com>
2026-01-09 10:09:45 -0300

    feat(core): add transaction model

---
+++ src/Finat.Core/Enums/ETransactionType.cs
@@ -0,0 +1,7 @@
+﻿namespace Finat.Core.Enums;
+
+public enum ETransactionType
+{
+    Deposit = 1,
+    Withdraw = 2,
+}
---
+++ src/Finat.Core/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using Finat.Core.Enums;
+
+namespace Finat.Core.Models;
+
+public class Transaction
+{
+    public long Id { get; init; }
+    public string Title { get; set; } = string.Empty;
+
+    public DateTime CreatAt { get; init; } = DateTime.UtcNow;
+    public DateTime? PaidOrReceivedAt { get; set; }
+
+    public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
+
+    public decimal Amount { get; set; }
+
+    public long CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+
+    public string UserId { get; set; } =  string.Empty;
+}
