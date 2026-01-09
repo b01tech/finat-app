c42ca36e4e848460d3967c1aeac7aff80191eb1a
b01tech <bruno01.tech@gmail.com>
2026-01-09 14:14:55 -0300

    refactor: change response properties

--- src/Finat.Core/Responses/Response.cs
+++ src/Finat.Core/Responses/Response.cs
@@ -5,13 +5,13 @@ public class Response<T>
     private readonly int _code;
 
     public T? Data { get; set; }
-    public List<string> Errors { get; set; } = new();
+    public List<string>? Errors { get; set; }
 
     public bool IsSuccess => _code is >= 200 and <= 299;
 
     protected Response() { }
 
-    public Response(int code, T? data, List<string> errors)
+    public Response(int code, T? data = default, List<string>? errors = null)
     {
         _code = code;
         Data = data;
