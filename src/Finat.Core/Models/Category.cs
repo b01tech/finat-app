553a5b9761783a829dff329a41f49b7262e61b67
b01tech <bruno01.tech@gmail.com>
2026-01-09 10:09:19 -0300

    feat(core): add category model

---
+++ src/Finat.Core/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Finat.Core.Models;
+
+public class Category
+{
+    public long Id { get; init; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+}
